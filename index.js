09df8dbd1b696da5253320fa88c5b5917368b39e
Muhammad Abid204 <decabidrafiq2008@gmail.com>
2025-03-07 01:26:59 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,66 @@
+console.log("hello world")
+import express from "express"
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send("hello world")
+})
+
+app.listen(port, () => {
+    console.log("example app listening on port ${port}")
+})
+
+import fs from 'fs'
+fs.readFileSync
+fs.readFileSync
+
+const http = require("http")
+import http from "http "
+
+import http from "http "
+
+
+http.createServer((req, res) => {
+    res.end("hello from serva")
+})
+
+Server.listen(8000, 127, 0.01, () => {
+    console.log("server is running on port 8000")
+})
+
+const url = require("url")
+import url from "url"
+
+console.log("req.url")
+
+if (req.url == '/') {
+    res.end("kya chaiye")
+}
+
+if (req.url == '/') {
+    res.end("allo papri")
+}
+
+const express = express()
+app.listen(8000, ()=> {
+    console.log("running server")
+})
+
+app.get('/',(req,res)=> {
+res.send("kay chiye")
+})
+
+app.get('/choleg',(req,res)=> {
+    res.send("ye lo choley")
+    })
+    
+
+
+
+
+
+
+
+
+
